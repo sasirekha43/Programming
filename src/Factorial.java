2484e5b4a55a327584c250cca5baee5c31e3efe6
sasirekha <sasirekha.sekhar@a-cti.com>
2018-02-02 03:32:16 +0530

    Fibonacci

---
+++ src/Factorial.java
@@ -0,0 +1,11 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int i, number = 4;
+        int fact =1;
+        for(i =1; i<=number;i++){
+            fact = fact * i;
+        }
+        System.out.print("Factorial of "+number+" is: "+fact);
+    }
+
+}
