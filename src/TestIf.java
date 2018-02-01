5a26683bed67d583b3afb2c373b6ce74657cb72d
sasirekha <sasirekha.sekhar@a-cti.com>
2018-02-02 03:59:07 +0530

    TestIF

---
+++ src/TestIf.java
@@ -0,0 +1,11 @@
+public class TestIf {
+    public static void main(String[] args) {
+        int age=32;
+        if(age>20){
+            System.out.print("age is greater than 20");
+        }
+        else {
+            System.out.print("age is less than 20");
+        }
+    }
+}
