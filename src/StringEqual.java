e8db2ec88d5426c15861a450943a50a4e4e2815f
sasirekha <sasirekha.sekhar@a-cti.com>
2018-02-04 23:52:30 +0530

    StringEqual

--- src/StringEqual.java
+++ src/StringEqual.java
@@ -2,7 +2,7 @@ public class StringEqual {
     public static void main(String[] args) {
         String s1 ="nani";
         String s2 ="sasi";
-        boolean b =s1.equals(s2);
+        boolean b = s1.equals(s2);
         System.out.println("This is equal method in string is"+b+"because it is different");
     }
 
