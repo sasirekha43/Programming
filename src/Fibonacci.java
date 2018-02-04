d1a628ce67f36f0acf208e4aed30e23d6aeb529c
sasirekha <sasirekha.sekhar@a-cti.com>
2018-02-04 06:36:04 +0530

    FibonacciNum

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,12 +1,15 @@
 public class Fibonacci {
     public static void main(String[] args) {
-        int n1 = 0, n2 = 1, n3, i, count = 10;
-        System.out.print(n1 + " " + n2);
-        for (i = 2; i < count; ++i) {
-            n3 = n1 + n2;
-            System.out.print(" " + n3);
-            n1 = n2;
-            n2 = n3;
+        int a=0, b=1, c;
+        System.out.println(a);
+        System.out.println(b);
+        c=a+b;
+        System.out.println(c);
+        while (c<15){
+            a=b;
+            b=c;
+            c=a+b;
+            System.out.println(c);
         }
     }
 }
