f87de1df970de5a31434af100476bdb30ebe7ea2
sasirekha <sasirekha.sekhar@a-cti.com>
2018-02-04 22:34:09 +0530

    ArithmeticOp

---
+++ src/ArithmeticOp.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class ArithmeticOp {
+    public static void main(String[] args) {
+        {
+            int m1,m2,m3, total; m1=25;m2=25; m3=25;
+            float avg;
+            total = m1+m2+m3;
+            avg= total/3;
+            System.out.println("total marks are "+total);
+            System.out.println("Average is:"+avg);
+        }
+    }
+
+}
