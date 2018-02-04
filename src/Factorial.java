cdfcad9c635f1401c45158a45d17ec629142522f
sasirekha <sasirekha.sekhar@a-cti.com>
2018-02-04 06:38:56 +0530

    FactorialNum

--- src/Factorial.java
+++ src/Factorial.java
@@ -2,6 +2,7 @@ public class Factorial {
     public static void main(String[] args) {
         int i, number = 4;
         int fact =1;
+
         for(i =1; i<=number;i++){
             fact = fact * i;
         }
