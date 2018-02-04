40bee158101704f96c83c23bd8ce708c9779eaa6
sasirekha <sasirekha.sekhar@a-cti.com>
2018-02-04 23:39:09 +0530

    StringConcat

--- src/ScannerStringApp.java
+++ src/ScannerStringApp.java
@@ -6,5 +6,5 @@ public class ScannerStringApp {
         String ch = sc.next();
         System.out.println("the give string value="+ch);
     }
-    
+
 }
--- src/StringConcat.java
+++ src/StringConcat.java
@@ -4,7 +4,7 @@ public class StringConcat {
         String s2 ="Sasi";
         String s3= s1.concat(s2);
         System.out.println("This is only string one=" +s1);
-        System.out.println("This is only string two="+s2);
+        System.out.println("This is only string two=" +s2);
         System.out.println("This is string Concatenation Method: " +s3);
     }
 }
