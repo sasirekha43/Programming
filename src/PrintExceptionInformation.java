0e97bf17c81adfa49f379a9b54623960cb1dbdff
sasirekha <sasirekha.sekhar@a-cti.com>
2018-02-07 15:39:20 +0530

    TryMultipleCatchBlocks

---
+++ src/PrintExceptionInformation.java
@@ -0,0 +1,13 @@
+public class PrintExceptionInformation {
+    public static void main(String[] args) {
+        try{
+            System.out.println(10/0);
+        }
+        catch (ArithmeticException e){
+            e.printStackTrace();
+            System.out.println(e.toString());
+            System.out.println(e.getMessage());
+
+        }
+    }
+}
